83c023741ecd3f2dc50f1a84786e18ab2e7db960
Pam Tholan <pamelatholan@yahoo.com>
2019-05-24 23:58:28 -0400

    coded game questions, answers and answer images, added images

---
+++ assets/javascript/app.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+// Questions
+var options = [
+    {
+        question: "What are 'Durin's folk' more commonly known as?",
+        choice: ["Dwarves", "Hobbits", "Forrest Elves", "Great Eagles"],
+        answer: 0,
+        photo: "assets/images/gimli.gif"
+    },
+    {
+        question: "What is the bridge of Khazad-dûm?",
+        choice: ["The bridge over the Brandywine River at the edge of The Shire", "The mythical gateway between Middle Earth and Valinor", "The bridge inside Moria where Gandolf fought the Belrog", "The entrance to Mirkwood"],
+        answer: 2,
+        photo: "assets/images/gandolf.gif"
+    },
+    {
+        question: "What Sindarin word was Aragorn known by when he was a child?",
+        choice: ["Amdir", "Estel", "Miriel", "Aeluin"],
+        answer: 1,
+        photo: "assets/images/aragorn.gif"
+    },
+    {
+        question: "What are Morgul wounds?",
+        choice: ["What Uruk-hai are born from", "Seige towers used by Orcs", "A wound inflicted by an Elf", "Wounds inflicted by the Nazgûl"],
+        answer: 3,
+        photo: "assets/images/nazgul.gif"
+    },
+    {
+        question: "What are the Palantíri and how many are there?",
+        choice: ["Seeing stones; 7", "Rings; 3", "Jewels; 5", "Elvish blades; 2"],
+        answer: 0,
+        photo: "assets/images/stone.gif"
+    },
+    {
+        question: "What is the name of the Inn where Aragorn meets the Hobbits?",
+        choice: ["The Green Dragon", "The Prancing Pony", "The Golden Peach", "The Ivy"],
+        answer: 1,
+        photo: "assets/images/inn.gif"
+    },
+    {
+        question: "What was Gollum's name before he found the ring?",
+        choice: ["Phil", "Bilbo", "Smeagol", "Deagol"],
+        answer: 2,
+        photo: "assets/images/smeagol.gif"
+    },
+    {
+        question: "How many rings of power were gifted to the race of men?",
+        choice: [4, 2, 8, 9],
+        answer: 3,
+        photo: "assets/images/rings.gif"
+    },
+    {
+        question: "What is the name of Gandolf's horse?",
+        choice: ["Brego", "Shadowfax", "Shadowmane", "Bill"],
+        answer: 1,
+        photo: "assets/images/shadowfax.gif"
+    },
+    {
+        question: "By what name do the Elves call Gandolf?",
+        choice: ["Mithrandir", "Gandolf the Grey", "Gandolf the White", "Tharkûn"],
+        answer: 0,
+        photo: "assets/images/mithrandir.gif"
+    }];
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// encapsulates entire game code    
+})
