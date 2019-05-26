8a1e1ed8a4f35c715595c8c583bc725f2b48279e
Pam Tholan <pamelatholan@yahoo.com>
2019-05-25 20:28:01 -0400

    timer now working, but choices not displaying for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,60 +5,70 @@ var options = [
         question: "What are 'Durin's folk' more commonly known as?",
         choice: ["Dwarves", "Hobbits", "Forrest Elves", "Great Eagles"],
         answer: 0,
+        shown: false,
         photo: "assets/images/gimli.gif"
     },
     {
         question: "What is the bridge of Khazad-dûm?",
         choice: ["The bridge over the Brandywine River at the edge of The Shire", "The mythical gateway between Middle Earth and Valinor", "The bridge inside Moria where Gandolf fought the Belrog", "The entrance to Mirkwood"],
         answer: 2,
+        shown: false,
         photo: "assets/images/gandolf.gif"
     },
     {
         question: "What Sindarin word was Aragorn known by when he was a child?",
         choice: ["Amdir", "Estel", "Miriel", "Aeluin"],
         answer: 1,
+        shown: false,
         photo: "assets/images/aragorn.gif"
     },
     {
         question: "What are Morgul wounds?",
         choice: ["What Uruk-hai are born from", "Seige towers used by Orcs", "A wound inflicted by an Elf", "Wounds inflicted by the Nazgûl"],
         answer: 3,
+        shown: false,
         photo: "assets/images/nazgul.gif"
     },
     {
         question: "What are the Palantíri and how many are there?",
         choice: ["Seeing stones; 7", "Rings; 3", "Jewels; 5", "Elvish blades; 2"],
         answer: 0,
+        shown: false,
         photo: "assets/images/stone.gif"
     },
     {
         question: "What is the name of the Inn where Aragorn meets the Hobbits?",
         choice: ["The Green Dragon", "The Prancing Pony", "The Golden Peach", "The Ivy"],
         answer: 1,
+        shown: false,
         photo: "assets/images/inn.gif"
     },
     {
         question: "What was Gollum's name before he found the ring?",
         choice: ["Phil", "Bilbo", "Smeagol", "Deagol"],
         answer: 2,
+        shown: false,
         photo: "assets/images/smeagol.gif"
     },
     {
         question: "How many rings of power were gifted to the race of men?",
         choice: [4, 2, 8, 9],
         answer: 3,
+        shown: false,
         photo: "assets/images/rings.gif"
     },
     {
         question: "What is the name of Gandolf's horse?",
         choice: ["Brego", "Shadowfax", "Shadowmane", "Bill"],
         answer: 1,
+        shown: false,
         photo: "assets/images/shadowfax.gif"
     },
     {
         question: "By what name do the Elves call Gandolf?",
         choice: ["Mithrandir", "Gandolf the Grey", "Gandolf the White", "Tharkûn"],
         answer: 0,
+        shown: false,
         photo: "assets/images/mithrandir.gif"
     }];
 
@@ -69,7 +79,7 @@ var options = [
     var intervalId;
     var userGuess = "";
     var running = false;
-    var qCount = options.length;
+    var qCount = 0;
     var pick;
     var index;
     var newArray = [];
@@ -118,14 +128,14 @@ var options = [
     function displayQuestion() {
         index = Math.floor(Math.random() * options.length);
         pick = options[index];
-        if(pick.shown) {
-            displayQuestion();
-        } else {
+        // if(pick.shown) {
+            // displayQuestion();
+        // } else {
             // console.log(pick.question);
-        }
+        // }
         $("#questionblock").html("<h2>" + pick.question + "</h2>");
         for(var i = 0; i < pick.choice.length; i++) {
-            var userChoice = $("#<div>");
+            var userChoice = $("");
             userChoice.addClass("answerChoice");
             userChoice.html(pick.choice[i]);
             //check answer
